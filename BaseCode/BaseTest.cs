1afaeaffb1050de3278b9483e7b843e5ec7ccc28
Ravi Pal <ravi.pal@sportzinteractive.net>
2020-01-08 10:35:24 +0530

    Base Project

---
+++ BaseCode/BaseTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BaseCode
+{
+    public class BaseTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+            Assert.Pass();
+        }
+    }
+}
